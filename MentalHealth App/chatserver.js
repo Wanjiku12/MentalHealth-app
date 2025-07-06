59a9d2cd9bc09ae508a20db1e6ce42dca27dbff6
WaweruMauree <maureen.waweru@strathmore.edu>
2025-07-06 23:07:45 +0300

    Adding mood tracker page

--- MentalHealth App/chatserver.js
+++
@@ -1,7 +0,0 @@
-Run this command to setup the project: 'git clone https://github.com/Wanjiku12/MentalHealth-app.git'
-Run this to fetch the latest updates: 'git fetch --all'
-Run this to setup the branch with latest changes: 'git checkout Freshia1'
-Copy the files in this zip file (chat_dashboard.html, server.js and package.json) into the project folder
-Confirm that you want to replace package.json in folder if it asks
-Run this to add the code with chat functionality: 'git add chat_dashboard.html server.js package.json'
-Run this to push the latest changes: 'git push'
